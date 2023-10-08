4984f56eb2cdff00988e7af186268bf5d65e689b
Julia Grimes <julia.s.grimes@gmail.com>
2023-10-08 14:13:00 -0400

    Add functions for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,42 @@
 class PostsController < ApplicationController
+
+  def index
+    posts = Post.all
+    render json: posts.as_json
+  end
+
+  def show
+    posts = Post.find_by(id: params[:id])
+  end
+
+  def create
+    post = Post.new(
+      header: params[:header],
+      date: params[:date],
+      
+    )
+    if post.save
+
+    render json: post.as_json
+      
+    else
+      render json: {errors: post.errors.full_messages},
+      status :unprocessable_entity
+    end
+      
+    end
+
+    def update
+      post = Post.find_by(id: params{:id})
+
+      post[:header] = params[:header] || post[:header]
+      post[:date] = params[:date] || post[:date]
+     
+    end
+
+    def destroy
+      post = Post.find(params[:id])
+      post.destroy
+    end
+
 end
