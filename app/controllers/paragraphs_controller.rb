7d6806006c77afbc916732d1b8d5d80c609b177f
Julia Grimes <julia.s.grimes@gmail.com>
2023-10-08 22:08:06 -0400

    update paragraphs controller

--- app/controllers/paragraphs_controller.rb
+++ app/controllers/paragraphs_controller.rb
@@ -1,2 +1,44 @@
 class ParagraphsController < ApplicationController
+  def index
+    paragraphs = Paragraph.all
+    render json: paragraphs.as_json
+  end
+
+  def show
+    @paragraphs = Paragraph.where(post_id: params[:id])
+    render json: @paragraphs.as_json
+
+  end
+
+  def create
+    paragraph = Paragraph.new(
+      writing: params[:writing],
+      post_id: params[:post_id],
+    )
+    if photo.save
+
+    render json: photo.as_json
+      
+    else
+      render json: {errors:photo.errors.full_messages},
+      status :unprocessable_entity
+    end
+      
+    end
+
+    def update
+      photo = Photo.find(params{:id})
+
+      photo[:url] = params[:url] || photo[:url]
+      photo[:name] = params[:name] || photo[:name]
+      photo[:abovewriting] = params[:abovewriting] || photo[:abovewriting]
+      photo[:belowwriting] = params[:belowwriting] || photo[:belowwriting]
+    end
+
+    def destroy
+      photo = Photo.find(params[:id])
+      photo.destroy
+    end
+
+
 end
